50e9dd9cf8d6d63102969615612e339c530594b0
NakamuraTokuhisa <pole0500tngwz@gmail.com>
2026-03-01 23:33:48 +0900

    追加　Springプロジェクトの基盤を構築

---
+++ toku2001/src/main/java/com/github/toku2001/Toku2001Application.java
@@ -0,0 +1,13 @@
+package com.github.toku2001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Toku2001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Toku2001Application.class, args);
+	}
+
+}
---
+++ toku2001/src/test/java/com/github/toku2001/Toku2001ApplicationTests.java
@@ -0,0 +1,12 @@
+package com.github.toku2001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Toku2001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
