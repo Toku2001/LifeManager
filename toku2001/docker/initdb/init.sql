ee166477b1073d3260713fc22abdb7c1bb902d71
NakamuraTokuhisa <pole0500tngwz@gmail.com>
2026-03-04 23:39:13 +0900

    削除　Dockerの使用を中止(Spring&SQLiteでプロジェクト作成するため)

--- toku2001/docker/initdb/init.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS health_auto_exports (
-    id INTEGER PRIMARY KEY,
-    setting_key TEXT NOT NULL UNIQUE,
-    status Integer NOT NULL,
-    created_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
-
-INSERT OR IGNORE INTO health_auto_exports (id, setting_key, status)
-VALUES (1, 'dummyKey', 1);
